b8a0a46bb1f41b16732e30d6bc4a4e3d9b3346ca
PerezYuan <yzmspirit@sina.cn>
2016-10-26 16:21:42 +0800

    merge some function

--- src/main.js
+++ src/main.js
@@ -1,31 +1,28 @@
 let Vue = require('vue');
 
 var app = new Vue({
-  el: '#app-1',
-  data: {
-    message: 'Hello Vue.js!'
-  }
-})
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date(),
-    seen : true,
-    todos : [
-        {text : 'list1'},
-        {text : 'list2'},
-        {text : 'list3'}
-    ]
-  }
+    el: '#app',
+    data: {
+        titleStr: 'You loaded this page on ' + new Date(),
+        message : 'reverse message',
+        seen : true,
+            todos : [
+            {text : 'list1'},
+            {text : 'list2'},
+            {text : 'list3'}
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        },
+        addOne : function () {
+            this.todos.push({text : 'list' + (this.todos.length + 1)});
+        }
+    }
 })
 
 var toggleBtn = document.getElementById('toggle');
 toggleBtn.onclick = function () {
-    app2.seen = !app2.seen;
-}
-
-var addOneBtn = document.getElementById('addOne');
-addOneBtn.onclick = function () {
-    app2.todos.push({text : 'list' + (app2.todos.length + 1)});
+    app.seen = !app.seen;
 }
